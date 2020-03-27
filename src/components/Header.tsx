64221a7108a2a6335b91004c1b32761848d1b12a
nerikosans <36780394+nerikosans@users.noreply.github.com>
2020-03-27 23:02:03 +0900

    Add Description

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,12 +13,17 @@ const Header: React.FC<HeaderProps> = () => {
       <SubTitle>Author: @nerikosans</SubTitle>
 
       <GithubButton />
+      <Description>
+        {
+          'musicxmlに含まれるアルファベットの歌詞をひらがな表記に変換して、NEUTRINOで読み込めるようにします。\n開発は非公式であり、NEUTRINO本体の開発元とは関係しておりません。'
+        }
+      </Description>
     </Wrapper>
   );
 };
 
 const Wrapper = styled(Container)`
-  margin: 3rem 0;
+  margin: 3rem 0 1rem;
   text-align: center;
 `;
 
@@ -31,6 +36,15 @@ const SubTitle = styled(Typography).attrs({
   variant: 'subtitle1',
 })`
   color: #ffffff;
+`;
+
+const Description = styled(Typography).attrs({
+  variant: 'body2',
+})`
+  color: #ffffff;
+  margin: 1rem auto 0;
+  width: 60%;
+  white-space: pre-line;
 `;
 
 export default Header;
