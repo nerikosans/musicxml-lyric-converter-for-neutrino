190e278cd2b648f371313bac3fd8efe77ad2c4d0
nerikosans <36780394+nerikosans@users.noreply.github.com>
2020-03-27 22:50:26 +0900

    Fix link

--- src/components/GithubButton.tsx
+++ src/components/GithubButton.tsx
@@ -5,8 +5,7 @@ const GithubButton: React.FC<{}> = () => {
   return (
     <GithubButtonWrapper>
       <iframe
-        src='https://ghbtns.com/github-btn.html?user=nerikosans&repo=musicxml-lyric-converter-for-neutrino
-      &type=star&count=true'
+        src='https://ghbtns.com/github-btn.html?user=nerikosans&repo=musicxml-lyric-converter-for-neutrino&type=star&count=true'
         frameBorder='0'
         scrolling='0'
         width='160px'
